c47d092956829895a75fb0a0ae75c57eec590abb
cameronaverill <cameronaverill2018@u.northwestern.edu>
2015-07-27 21:34:29 +0200

    added user specific actions

--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -1,4 +1,12 @@
 class ExpendituresController < ApplicationController
+  before_action :require_login
+
+  def require_login
+    unless user_signed_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to new_login_url # halts request cycle
+    end
+  end
 
   def index
     @expenditures = Expenditure.all.order(purchase_date: :desc)
@@ -10,6 +18,7 @@ class ExpendituresController < ApplicationController
 
   def create
     @expenditure = Expenditure.new(expenditure_params)
+    @expenditure.update user_id: current_user.id
     if @expenditure.save
       redirect_to expenditure_path(@expenditure)
     else
@@ -19,6 +28,27 @@ class ExpendituresController < ApplicationController
 
   def show
     @expenditure = Expenditure.find(params[:id])
+  end
+
+  def edit
+    @expenditure = Expenditure.find(params[:id])
+  end
+
+  def update
+    @expenditure = Expenditure.find(params[:id])
+
+    if @expenditure.update(expenditure_params)
+      redirect_to @expenditure
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @expenditure = Expenditure.find(params[:id])
+    @expenditure.destroy
+
+    redirect_to expenditures_path
   end
 
   private
